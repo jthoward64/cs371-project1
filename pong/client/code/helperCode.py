b8ce727633cb406e7e6b2f441f16662707fb8a4f
Joshua Tag Howard <tag@jthoward.dev>
2023-11-06 13:25:58 -0500

    Fix type error in helperCode.py

--- pong/client/code/helperCode.py
+++ pong/client/code/helperCode.py
@@ -1,13 +1,21 @@
 # You don't need to edit this file at all unless you really want to
 import pygame
 
+
 # This draws the score to the screen
-def updateScore(lScore:int, rScore:int, screen:pygame.surface.Surface, color, scoreFont:pygame.font.Font) -> pygame.Rect:
+def updateScore(
+    lScore: int,
+    rScore: int,
+    screen: pygame.surface.Surface,
+    color,
+    scoreFont: pygame.font.Font,
+) -> pygame.Rect:
     textSurface = scoreFont.render(f"{lScore}   {rScore}", False, color)
     textRect = textSurface.get_rect()
     screenWidth = screen.get_width()
-    textRect.center = ((screenWidth/2)+5, 50)
+    textRect.center = (((screenWidth / 2) + 5).__round__(), 50)
     return screen.blit(textSurface, textRect)
+
 
 class Paddle:
     def __init__(self, rect: pygame.Rect) -> None:
@@ -15,26 +23,27 @@ class Paddle:
         self.moving = ""
         self.speed = 5
 
+
 class Ball:
-    def __init__(self, rect:pygame.Rect, startXvel:int, startYvel:int) -> None:
+    def __init__(self, rect: pygame.Rect, startXvel: int, startYvel: int) -> None:
         self.rect = rect
         self.xVel = startXvel
         self.yVel = startYvel
         self.startXpos = rect.x
         self.startYpos = rect.y
-    
+
     def updatePos(self) -> None:
         self.rect.x += self.xVel
         self.rect.y += self.yVel
-    
-    def hitPaddle(self, paddleCenter:int) -> None:
+
+    def hitPaddle(self, paddleCenter: int) -> None:
         self.xVel *= -1
-        self.yVel = (self.rect.center[1] - paddleCenter)//2
-    
+        self.yVel = (self.rect.center[1] - paddleCenter) // 2
+
     def hitWall(self) -> None:
         self.yVel *= -1
-    
-    def reset(self, nowGoing:str) -> None:
+
+    def reset(self, nowGoing: str) -> None:
         # nowGoing  The direction the ball should be going after the reset
         self.rect.x = self.startXpos
         self.rect.y = self.startYpos
