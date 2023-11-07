c9b2394b7f0df7f3e8820dca9e8be4dddf2e4d15
Joshua Tag Howard <tag@jthoward.dev>
2023-11-07 11:17:37 -0500

    Remove leaderboard helper

--- pong-v7/server/main.py
+++ pong-v7/server/main.py
@@ -1,9 +1,6 @@
 from code.lobby import LobbyServer
 
-from helpers.leaderboard import run_server
-
 if __name__ == "__main__":
     print("Starting Lobby Server")
-    run_server()
     run_game = LobbyServer()
     print("Lobby Server Closed")
