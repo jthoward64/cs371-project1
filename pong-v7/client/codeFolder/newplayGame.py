83ec4874b0834f6684577a9f1c20bebb11062b0c
Joshua Tag Howard <tag@jthoward.dev>
2023-11-17 14:42:56 -0500

    Add game_api to newPlayGame

--- pong-v7/client/codeFolder/newplayGame.py
+++ pong-v7/client/codeFolder/newplayGame.py
@@ -1,68 +1,100 @@
-import pygame
-import tkinter as tk
-import sys
 import socket
+import sys
+import time
+import tkinter as tk
+from tkinter import messagebox
 
+import pygame
 from helperCode import *
+
+from .api.gameApi import GameApi
+
+# Constants
+WHITE = (255, 255, 255)
+clock = pygame.time.Clock()
+scoreFont = pygame.font.Font("./assets/fonts/pong-score.ttf", 32)
+winFont = pygame.font.Font("./assets/fonts/visitor.ttf", 48)
+bottomFont = pygame.font.Font(None, 24)
+restartFont = pygame.font.Font(None, 32)
+pointSound = pygame.mixer.Sound("./assets/sounds/point.wav")
+bounceSound = pygame.mixer.Sound("./assets/sounds/bounce.wav")
+
 
 # This is the main game loop.  For the most part, you will not need to modify this.  The sections
 # where you should add to the code are marked.  Feel free to change any part of this project
 # to suit your needs.
-def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.socket, lInitial:str, rInitial:str) -> None:
-    
+def playGame(
+    screenWidth: int,
+    screenHeight: int,
+    playerPaddle: str,
+    game_api: GameApi,
+    lInitial: str,
+    rInitial: str,
+) -> None:
+    game_metadata = game_api.game_info()
+    if isinstance(game_metadata, str):
+        print("Server Disconnected")
+        pygame.quit()
+        return
+    game_code = game_metadata["game_code"]
+
     # Pygame inits
     pygame.mixer.pre_init(44100, -16, 2, 2048)
     pygame.init()
 
-    # Constants
-    WHITE = (255,255,255)
-    clock = pygame.time.Clock()
-    scoreFont = pygame.font.Font("./assets/fonts/pong-score.ttf", 32)
-    winFont = pygame.font.Font("./assets/fonts/visitor.ttf", 48)
-    bottomFont = pygame.font.Font(None, 24)
-    restartFont = pygame.font.Font(None, 32)
-    pointSound = pygame.mixer.Sound("./assets/sounds/point.wav")
-    bounceSound = pygame.mixer.Sound("./assets/sounds/bounce.wav")
-
     # Display objects
     screen = pygame.display.set_mode((screenWidth, screenHeight))
-    winMessage = pygame.Rect(0,0,0,0)
+    winMessage = pygame.Rect(0, 0, 0, 0)
 
-    topWall = pygame.Rect(-10,0,screenWidth+20, 10)
-    bottomWall = pygame.Rect(-10, screenHeight-40, screenWidth+20, 10)
+    topWall = pygame.Rect(-10, 0, screenWidth + 20, 10)
+    bottomWall = pygame.Rect(-10, screenHeight - 40, screenWidth + 20, 10)
 
-    bottomMessageSurface = bottomFont.render("Game Code: 0fmNq!", True, WHITE)
+    bottomMessageSurface = bottomFont.render(f"Game Code: {game_code}", True, WHITE)
     bottomtargetRect = pygame.Rect(0, screenHeight - 40, screenWidth, 50)
-    bottomtext_x = bottomtargetRect.x + (bottomtargetRect.width - bottomMessageSurface.get_width()) // 2
-    bottomtext_y = bottomtargetRect.y + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    bottomtext_x = (
+        bottomtargetRect.x
+        + (bottomtargetRect.width - bottomMessageSurface.get_width()) // 2
+    )
+    bottomtext_y = (
+        bottomtargetRect.y
+        + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    )
 
     restartText = restartFont.render("Press 'r' to restart", True, WHITE)
-    restartText_center = (screenWidth/2-100, screenHeight/2+30)
-    restartHolder = pygame.Rect(0,0,0,0)
+    restartText_center = (screenWidth / 2 - 100, screenHeight / 2 + 30)
+    restartHolder = pygame.Rect(0, 0, 0, 0)
 
     waitingText = restartFont.render("Waiting on other player...", True, WHITE)
-    waitingText_center = (screenWidth/2-125, screenHeight/2+30)
+    waitingText_center = (screenWidth / 2 - 125, screenHeight / 2 + 30)
 
     leftTextScore = bottomFont.render(f"{lInitial}: 0", True, WHITE)
     leftText_x = 10
-    leftText_y = bottomtargetRect.y + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    leftText_y = (
+        bottomtargetRect.y
+        + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    )
 
     rightTextScore = bottomFont.render(f"{rInitial}: 0", True, WHITE)
     rightText_x = 100
-    rightText_y = bottomtargetRect.y + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    rightText_y = (
+        bottomtargetRect.y
+        + (bottomtargetRect.height - bottomMessageSurface.get_height()) // 2
+    )
 
     centerLine = []
-    for i in range(0, screenHeight-40, 10):
-        centerLine.append(pygame.Rect((screenWidth/2)-5,i,5,5))
+    for i in range(0, screenHeight - 40, 10):
+        centerLine.append(pygame.Rect((screenWidth / 2) - 5, i, 5, 5))
 
     # Paddle properties and init
     paddleHeight = 50
     paddleWidth = 10
-    paddleStartPosY = (screenHeight/2)-(paddleHeight/2)
-    leftPaddle = Paddle(pygame.Rect(10,paddleStartPosY, paddleWidth, paddleHeight))
-    rightPaddle = Paddle(pygame.Rect(screenWidth-20, paddleStartPosY, paddleWidth, paddleHeight))
+    paddleStartPosY = (screenHeight / 2) - (paddleHeight / 2)
+    leftPaddle = Paddle(pygame.Rect(10, paddleStartPosY, paddleWidth, paddleHeight))
+    rightPaddle = Paddle(
+        pygame.Rect(screenWidth - 20, paddleStartPosY, paddleWidth, paddleHeight)
+    )
 
-    ball = Ball(pygame.Rect(screenWidth/2, screenHeight/2, 5, 5), -5, 0)
+    ball = Ball(pygame.Rect(screenWidth / 2, screenHeight / 2, 5, 5), -5, 0)
 
     # Set to false when we're not running
     running = True
@@ -83,12 +115,22 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
 
     sync = 0
 
+    # tester = None
+    # while not tester:
+    #     tester = game_api.start_game()
+    #     if isinstance(tester, str):
+    #         print(tester)
+    #         messagebox.showerror("Error", tester)
+    #         pygame.quit()
+    #         return
+    #     time.sleep(0.02)
+
     while True:
         # Wiping the screen
-        screen.fill((0,0,0))
+        screen.fill((0, 0, 0))
 
         # Blit the bottomMessage
-        gameMessage = screen.blit(bottomMessageSurface, (bottomtext_x, bottomtext_y)) 
+        gameMessage = screen.blit(bottomMessageSurface, (bottomtext_x, bottomtext_y))
         leftMessage = screen.blit(leftTextScore, (leftText_x, leftText_y))
         rightMessage = screen.blit(rightTextScore, (rightText_x, rightText_y))
 
@@ -110,24 +152,23 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
                     restart = True
 
             elif event.type == pygame.KEYUP:
-                playerPaddleObj.moving = ""
-
+                playerPaddleObj.moving = None
 
         # =========================================================================================
         # Your code here to send an update to the server on your paddle's information,
         # where the ball is and the current score.
         # Feel free to change when the score is updated to suit your needs/requirements
-        
+
         # Only send an update if the game is currently running
         if running:
             pass
-        
+
         # =========================================================================================
 
         # Update the player paddle and opponent paddle's location on the screen
         for paddle in [playerPaddleObj, opponentPaddleObj]:
             if paddle.moving == "down":
-                if paddle.rect.bottomleft[1] < screenHeight-10:
+                if paddle.rect.bottomleft[1] < screenHeight - 10:
                     paddle.rect.y += paddle.speed
             elif paddle.moving == "up":
                 if paddle.rect.topleft[1] > 10:
@@ -137,9 +178,9 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
         if lScore > 4 or rScore > 4 or running is False:
             # Add our win message
             winText = "Player 1 Wins! " if lScore > 4 else "Player 2 Wins! "
-            textSurface = winFont.render(winText, False, WHITE, (0,0,0))
+            textSurface = winFont.render(winText, False, WHITE, (0, 0, 0))
             textRect = textSurface.get_rect()
-            textRect.center = ((screenWidth/2), screenHeight/2)
+            textRect.center = (int(screenWidth / 2), int(screenHeight / 2))
             winMessage = screen.blit(textSurface, textRect)
 
             # Add our message to restart the game
@@ -151,7 +192,6 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
 
                 ################################################################ SEND TO SERVERF "START GAME" REQUEST #################################################################
         else:
-
             # ==== Ball Logic =====================================================================
             ball.updatePos()
 
@@ -164,7 +204,7 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
                 rScore += 1
                 pointSound.play()
                 ball.reset(nowGoing="right")
-                
+
             # If the ball hits a paddle
             if ball.rect.colliderect(playerPaddleObj.rect):
                 bounceSound.play()
@@ -172,12 +212,12 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
             elif ball.rect.colliderect(opponentPaddleObj.rect):
                 bounceSound.play()
                 ball.hitPaddle(opponentPaddleObj.rect.center[1])
-                
+
             # If the ball hits a wall
             if ball.rect.colliderect(topWall) or ball.rect.colliderect(bottomWall):
                 bounceSound.play()
                 ball.hitWall()
-            
+
             pygame.draw.rect(screen, WHITE, ball)
             # ==== End Ball Logic =================================================================
 
@@ -185,7 +225,7 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
         if running is True:
             for i in centerLine:
                 pygame.draw.rect(screen, WHITE, i)
-        
+
         # Drawing the player's new location
         for paddle in [playerPaddleObj, opponentPaddleObj]:
             pygame.draw.rect(screen, WHITE, paddle)
@@ -193,9 +233,23 @@ def playGame(screenWidth:int, screenHeight:int, playerPaddle:str, client:socket.
         pygame.draw.rect(screen, WHITE, topWall)
         pygame.draw.rect(screen, WHITE, bottomWall)
         scoreRect = updateScore(lScore, rScore, screen, WHITE, scoreFont)
-        pygame.display.update([topWall, bottomWall, ball, leftPaddle, rightPaddle, scoreRect, winMessage, gameMessage, restartHolder, leftMessage, rightMessage])
+        pygame.display.update(
+            [
+                topWall,
+                bottomWall,
+                ball,
+                leftPaddle,
+                rightPaddle,
+                scoreRect,
+                winMessage,
+                gameMessage,
+                restartHolder,
+                leftMessage,
+                rightMessage,
+            ]
+        )
         clock.tick(60)
-        
+
         # This number should be synchronized between you and your opponent.  If your number is larger
         # then you are ahead of them in time, if theirs is larger, they are ahead of you, and you need to
         # catch up (use their info)
